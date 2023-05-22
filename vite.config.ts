7348a38bd6997d5ee3168c7947857422265e7fa4
fuzj <1243809347@qq.com>
2023-05-22 13:38:35 +0800

    更改配置

--- vite.config.ts
+++ vite.config.ts
@@ -10,19 +10,20 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+    },
+    dedupe: ['vue']
   },
   build: {
     outDir: 'lib',
-      lib: {
+    lib: {
       entry: resolve(__dirname, './packages/index.ts'),
         name: 'WebVue',
         fileName: 'web-vue'
     },
     rollupOptions: {
-      // 确保外部化处理那些你不想打包进库的依赖
+      // 确保外部化处理那些你不想打包进库的依赖,解决插件报错问题(reading 'isCE')
       external: ['vue'],
-        output: {
+      output: {
         // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
         globals: {
           vue: 'Vue'
